7e739c9ec5fbcd9fe088b4d646d7ada827ca4808
shakhbozmn <shakhbozmn@gmail.com>
2024-12-01 03:15:26 +0500

    ⚙️ fix: Update delete confirmation messages for cars and rentals to improve clarity; remove optional chaining

--- back/CarRentalSystem_00016395/obj/Debug/net6.0/CarRentalSystem_00016395.AssemblyInfo.cs
+++ back/CarRentalSystem_00016395/obj/Debug/net6.0/CarRentalSystem_00016395.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRentalSystem_00016395")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64e4256d6bf8255e29926d05b8a7260823498f26")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRentalSystem_00016395")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRentalSystem_00016395")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
